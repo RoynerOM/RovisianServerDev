a3ec7575780f674fd5cfe018cfafba043f974d3a
Royner OM <roportameza@gmail.com>
2024-03-09 15:55:47 -0600

    Estructura de proyecto
    Se creo una estructura en el cual se trata de aplicar arquitectura limpia en un api rest con asp.net core 6

---
+++ RovisianServerDev.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace RovisianServerDev.IntegrationTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
